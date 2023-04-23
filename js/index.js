59136aa36ea1146025fc18ad6e7c3a770eefb2dc
reymond Diaz <reymonddiaz959@gmail.com>
2023-04-23 14:27:23 -0500

    modificando estilos

--- js/index.js
+++ js/index.js
@@ -1,38 +1,46 @@
 let inputMensaje = document.querySelector("#mensaje");
 let inputResultado = document.querySelector("#resultado");
-
 let btnEncriptar = document.querySelector("#encriptar");
 let btnDesencriptar = document.querySelector("#desencriptar");
 let btncopiar = document.querySelector("#copiar");
 
 
 function encriptar (){
-    let texto = document.getElementById("mensaje")
-    var mensaje = inputMensaje.value;
-    var mensajeEncriptado =  mensaje
     
+    let texto = document.getElementById("mensaje")
+    let mensaje = inputMensaje.value;
+    let mensajeEncriptado =  mensaje.toLowerCase()
     .replaceAll("e","enter")
     .replaceAll("i","imes")
     .replaceAll("o","ober")
     .replaceAll("a","ai") 
-    .replaceAll("u","ufat")
-    
+    .replaceAll("u","ufat");
 
+    document.getElementById("contenedor-2-inicio","imagen").style.visibility = "hidden";
+    document.getElementById("contenedor-3").style.visibility = "visible";
     inputResultado.value = mensajeEncriptado;
+    document.getElementById("mensaje").value = "";
+    
+   
 }
+
 function desencriptar (){
-    var encriptado = inputMensaje.value;
-    var mensaje =  encriptado
+    let encriptado = inputMensaje.value;
+     mensaje =  encriptado.toLowerCase()
     
     .replaceAll("enter","e")
     .replaceAll("imes","i")
     .replaceAll("ober","o")
     .replaceAll("ai","a")
-    .replaceAll("ufat","u")
-   
-   
+    .replaceAll("ufat","u");
     inputResultado.value = mensaje;
+    document.getElementById("contenedor-2-inicio","imagen").style.visibility = "hidden";
+    document.getElementById("contenedor-3").style.visibility = "visible";
+    document.getElementById("mensaje").value = "";
+   
+   
 }
+ 
 
 function copiar(){
     let mensajeEncriptado = inputResultado.value;
@@ -43,4 +51,3 @@ function copiar(){
 btnEncriptar.onclick = encriptar;
 btnDesencriptar.onclick = desencriptar;
 btncopiar.onclick = copiar;
-
